f9fb7960284703def0e1826fc5467768866a3844
Leon Vogler <leonfvogler@gmail.com>
2022-07-24 15:18:47 +0200

    update actionOptions

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -20,12 +20,9 @@ module.exports = ({ env }) => ({
         },
       },
       actionOptions: {
-        upload: {
-          ACL: null
-        },
-        uploadStream: {
-          ACL: null
-        },
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
       breakpoints: {
         xlarge: 2000,
