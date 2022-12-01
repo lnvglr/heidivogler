6b1dcf9313b8edbd91b4d715f4cc3c2187a5daf9
Leon Vogler <leonfvogler@gmail.com>
2022-12-01 16:22:54 +0200

    add .DS_Store to gitignore

--- frontend/layouts/generic.vue
+++ frontend/layouts/generic.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col min-h-screen overflow-hidden">
     <main class="relative top-36 mb-36 max-w-4xl mx-auto w-full px-12">
-      <h1 v-if="$slots.title" class="font-bold text-left text-4xl sm:text-6xl my-24 hyphens-auto">
+      <h1 v-if="$slots.title" class="font-bold text-left text-4xl sm:text-6xl my-24 hyphens-auto leading">
         <slot name="title" />
       </h1>
       <div class="pb-36">
