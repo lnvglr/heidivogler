f3a948ddd0f41dbcc4da7524236367b8ca362c20
Leon Vogler <leonfvogler@gmail.com>
2022-12-04 23:22:04 +0200

    fix: comma

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
 				as: 'font',
 				type: 'font/woff2',
 				crossorigin: true
-			}
+			},
 			{
 				rel: 'preload',
 				href: '/fonts/Gramatika-Regular.woff2',
