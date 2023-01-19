f84fab5633ce7b3d389b73b8c2830c07aec633d8
Leon Vogler <leonfvogler@gmail.com>
2023-01-19 17:28:45 +0200

    pupulate priceGroup

--- backend/src/api/offer/controllers/offer.js
+++ backend/src/api/offer/controllers/offer.js
@@ -25,6 +25,7 @@ module.exports = createCoreController('api::offer.offer', ({ strapi }) => ({
 					populate: {
 						image: true,
 						tiles: true,
+						priceGroup: true,
 						tile: true,
 						imageTextBlock: true,
 					},
