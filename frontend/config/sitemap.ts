57390a641669d7e8bfe8cfbc6f6f0560cb4f1a62
Leon Vogler <leonfvogler@gmail.com>
2023-04-16 16:50:07 +0300

    update sitemap generation

--- frontend/config/sitemap.ts
+++ frontend/config/sitemap.ts
@@ -1,19 +1,19 @@
 import { Offer } from '~/types'
 import { Strapi4ResponseMany } from '@nuxtjs/strapi/dist/runtime/types'
 import fetch from 'node-fetch'
-const siteUrl = process.env.API_BASE || 'http://localhost:1337'
+const apiBase = process.env.API_BASE || 'http://localhost:1337'
+const siteUrl = process.env.NUXT_PUBLIC_SITE_URL
 
 export default {
-	// provide dynamic URLs to be included
 	siteUrl,
 	urls: async () => {
 		try {
-			const res = await fetch(`${siteUrl}/api/offers`)
+			const res = await fetch(`${apiBase}/api/offers`)
 			const { data } = (await res.json() as Strapi4ResponseMany<Offer>)
 			return (data).map((offer) => ({
 				loc: `/angebot/${offer.attributes.slug}`,
 				lastmod: offer.attributes.updatedAt,
-				changefreq: 'daily',
+				changefreq: 'weekly',
 				priority: 0.8,
 			}))
 		} catch (error) {
