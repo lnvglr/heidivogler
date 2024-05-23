5b9ff5983bcf1199c043992d556c2b5d605954d6
Leon Vogler <leonfvogler@gmail.com>
2024-05-23 11:50:13 +0100

    update cors

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -31,7 +31,7 @@ module.exports = [
     config: {
       enabled: true,
       headers: '*',
-      origin: ['http://localhost:1337', process.env.NUXT_PUBLIC_SITE_URL],
+      origin: process.env.NODE_ENV === 'development' ? ['http://localhost:1337'] : [process.env.API_BASE, process.env.NUXT_PUBLIC_SITE_URL],
     }
   },
   'strapi::logger',
