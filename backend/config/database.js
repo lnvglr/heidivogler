4fc77784f7a6419ed8e9a5071749f181a857e43a
Leon Vogler <leonfvogler@gmail.com>
2026-04-14 12:03:52 +0200

    chore: update database configuration to use environment variables instead of connection string parsing

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,15 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
+// const parse = require('pg-connection-string').parse;
+// const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host: env('PGHOST'),
+      port: env('PGPORT'),
+      database: env('PGDATABASE'),
+      user: env('PGUSER'),
+      password: env('PGPASSWORD'),
       ssl: {
         rejectUnauthorized: false
       },
